d5367c017ebd6df10bd154f3d9a40e75a311c824
Mimi Schatz <mcschatz@gmail.com>
2015-12-21 11:59:07 -0500

    moved methods to body so allskills and newskill could access. new skills now added immeditately to the DOM

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,12 +1,25 @@
 var Body = React.createClass({
+  getInitialState() {
+    return { skills: [] }
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
+  handleSubmit(skill) {
+    let newState = this.state.skills.concat(skill);
+    this.setState({ skills: newState })
+  },
+
   render() {
     return (
       <div>
         <div>
-          <NewSkill />
+          <NewSkill handleSubmit={this.handleSubmit} />
         </div>
         <div>
-          <AllSkills />
+          <AllSkills skills={this.state.skills} />
         </div>
       </div>
     )
