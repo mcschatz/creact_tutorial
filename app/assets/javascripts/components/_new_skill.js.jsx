1004f44786baaba9f461244d84a25c433f8afd87
Mimi Schatz <mcschatz@gmail.com>
2015-12-21 11:38:52 -0500

    added fields for new skill

--- app/assets/javascripts/components/_new_skill.js.jsx
+++ app/assets/javascripts/components/_new_skill.js.jsx
@@ -2,7 +2,9 @@ var NewSkill = React.createClass({
   render() {
     return (
       <div>
-        <h1>Mimi Schatz</h1>
+        <input ref='name' placeholder='Enter name of skill' />
+        <input ref='details' placeholder='Details' />
+        <button>Submit</button>
       </div>
     )
   }
