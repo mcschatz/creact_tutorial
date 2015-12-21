00728bf8c91a9cc5b860608626baf17e422ee9ea
Mimi Schatz <mcschatz@gmail.com>
2015-12-21 12:54:57 -0500

    added edit to skill component

--- app/assets/javascripts/components/_skill.js.jsx
+++ app/assets/javascripts/components/_skill.js.jsx
@@ -1,17 +1,42 @@
 var Skill = React.createClass({
+  getInitialState() {
+    return { editable: false }
+  },
+
+  handleEdit() {
+    if (this.state.editable) {
+      let name = this.refs.name.value;
+      let details = this.refs.details.value;
+      console.log('in handleEdit', this.state.editable, name, details);
+    }
+
+    this.setState({ editable: !this.state.editable })
+  },
+
   render() {
+    var name = this.state.editable ? <input type='text'
+                                            ref='name'
+                                            defaultValue={this.props.skill.name} />
+                                   : <h3>{this.props.skill.name}</h3>
+
+    let details = this.state.editable ? <textarea type='text'
+                                                  refs='details'
+                                                  defaultValue={this.props.skill.details}>
+                                        </textarea>
+                                      : <p>{this.props.skill.details}</p>
     return (
       <div>
-        <h3>{this.props.skill.name}</h3>
-        <p><strong>Level:</strong> {this.props.skill.level}</p>
-        <p>{this.props.skill.details}</p>
+      {name}
 
-        <button onClick={this.props.handleDelete}>
-          Delete
-        </button>
+      <p><strong>Level:</strong> {this.props.skill.level}</p>
+      {details}
 
-        <button onClick={this.props.handleEdit}>Edit</button>
-      </div>
+      <button onClick={this.props.handleDelete}>
+        Delete
+      </button>
+
+      <button onClick={this.handleEdit}>{this.state.editable ? 'Submit' : 'Edit'</button>
+    </div>
     )
   }
 });
